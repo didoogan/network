65544113bfdd6aada726c21d7a7fec5f3f440a75
didoogan <tzd0409@gmail.com>
2017-03-02 18:43:05 +0200

    create MyUser(AbstractUser)

--- network/settings.py
+++ network/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_auth',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL='my_auth.MyUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
