d0dc73d79fb28b0ea83b94cc88143e7828332b64
didoogan <tzd0409@gmail.com>
2017-03-03 18:43:19 +0200

    last commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'Home';
   email: string = "";
   password: string= "";
@@ -15,5 +15,8 @@ export class AppComponent {
   onSubmit() {
     console.log(this.email);
     console.log(this.password);
+  }
+  ngOnInit() {
+    console.log('in ngOninit of AppComponent');
   }
 }
--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -10,7 +10,7 @@ export class AuthComponent implements OnInit {
     title = "Lodin";
     email: string = "";
     password: string= "";
-
+    headers: any;
     constructor(private _authService: AuthService) { }
 
     logIn() {
@@ -20,6 +20,7 @@ export class AuthComponent implements OnInit {
         );
     }
     ngOnInit() {
+      console.log('in ngOnInit of Auth');
     }
 
 }
