53d826c0a59c971d0f7031ae71476f1881b661c7
didoogan <tzd0409@gmail.com>
2017-03-06 19:18:24 +0200

    begin Post model

--- post/models.py
+++ post/models.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.db import models
 
+
 class PostModel(models.Model):
-    text =
+    text = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
